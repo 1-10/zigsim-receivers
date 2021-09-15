30b0800622ac74054f12a3febbc5a4f10dd637d1
Yoneyama <yoneyama@1-10.com>
2021-09-15 17:05:32 +0900

    fix: settings variable

--- ARkit/BodyChoiceQuiz/assets/settings.js
+++ ARkit/BodyChoiceQuiz/assets/settings.js
@@ -1,8 +1,8 @@
 const settings = {
-    "width": 2.0,
-    "height": 2.0,
-    "heightAdjustValue": 1.0,
-    "widthAdjustValue": 0.0,
+    "xWidth": 2.0,
+    "zWidth": 2.0,
+    "xWidthAdjustValue": 0.0,
+    "zWidthAdjustValue": 1.0,
     "timeLimit": 5,
     "switchingTimeFromTimeLimit": 1.5,
     "switchingTimeFromCheckAnswer": 2
